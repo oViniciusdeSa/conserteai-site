1d0cec324f234a72e26f8fce52dd45058f3061f2
Vinicius de Sá <clicketenha10@gmail.com>
2026-07-17 15:31:10 -0300

    Adiciona SEO avançado e Schema LocalBusiness

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://conserteaí.com.br"),
+  metadataBase: new URL("https://www.xn--consertea-o5a.com.br"),
 
   title: {
     default: "ConserteAí | Desentupidora 24h em São Paulo",
@@ -38,11 +38,15 @@ export const metadata: Metadata = {
   creator: "ConserteAí",
   publisher: "ConserteAí",
 
+  alternates: {
+    canonical: "/",
+  },
+
   openGraph: {
     title: "ConserteAí | Desentupidora 24h em São Paulo",
     description:
       "Atendimento 24 horas para desentupimentos, caça vazamentos e emergências hidráulicas em São Paulo e Grande SP.",
-    url: "https://conserteaí.com.br",
+    url: "/",
     siteName: "ConserteAí",
     locale: "pt_BR",
     type: "website",
@@ -64,10 +68,130 @@ export const metadata: Metadata = {
     images: ["/logo-conserteai.png"],
   },
 
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/logo-conserteai.png",
+  },
+
   robots: {
     index: true,
     follow: true,
-},
+  },
+};
+
+const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Plumber",
+  "@id": "https://www.xn--consertea-o5a.com.br/#empresa",
+  name: "ConserteAí",
+  alternateName: "ConserteAí 24h",
+  description:
+    "Empresa especializada em desentupimento, caça vazamentos, limpeza de caixa de gordura e serviços hidráulicos emergenciais 24 horas.",
+  url: "https://www.xn--consertea-o5a.com.br",
+  logo: "https://www.xn--consertea-o5a.com.br/logo-conserteai.png",
+  image: "https://www.xn--consertea-o5a.com.br/logo-conserteai.png",
+
+  telephone: "+5511925331686",
+  email: "contatoconserteai24h@gmail.com",
+
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "Avenida Francisco Nóbrega Barbosa, 301",
+    addressLocality: "São Paulo",
+    addressRegion: "SP",
+    addressCountry: "BR",
+  },
+
+  openingHoursSpecification: [
+    {
+      "@type": "OpeningHoursSpecification",
+      dayOfWeek: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+      ],
+      opens: "00:00",
+      closes: "23:59",
+    },
+  ],
+
+  areaServed: [
+    {
+      "@type": "City",
+      name: "São Paulo",
+    },
+    {
+      "@type": "AdministrativeArea",
+      name: "Região do ABC Paulista",
+    },
+    {
+      "@type": "City",
+      name: "Guarulhos",
+    },
+    {
+      "@type": "AdministrativeArea",
+      name: "Região Metropolitana de São Paulo",
+    },
+  ],
+
+sameAs: [
+  "https://www.instagram.com/conserteai24h",
+  "https://www.facebook.com/profile.php?id=61591863178665",
+],
+  contactPoint: {
+    "@type": "ContactPoint",
+    telephone: "+5511925331686",
+    contactType: "customer service",
+    availableLanguage: "Portuguese",
+    areaServed: "BR",
+  },
+
+  hasOfferCatalog: {
+    "@type": "OfferCatalog",
+    name: "Serviços da ConserteAí",
+    itemListElement: [
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Desentupimento de esgoto",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Desentupimento de pia e vaso sanitário",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Caça vazamentos",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Limpeza de caixa de gordura",
+        },
+      },
+      {
+        "@type": "Offer",
+        itemOffered: {
+          "@type": "Service",
+          name: "Hidráulica emergencial",
+        },
+      },
+    ],
+  },
 };
 
 export default function RootLayout({

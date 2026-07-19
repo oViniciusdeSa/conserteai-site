8a34cd53719d4a196e6c9f1a4e02943e613b3132
Vinicius de Sá <clicketenha10@gmail.com>
2026-07-19 18:38:00 -0300

    Atualiza estrutura final do sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,4 @@
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 
 const baseUrl = "https://www.xn--consertea-o5a.com.br";
 
@@ -10,63 +10,54 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
-
     {
       url: `${baseUrl}/desentupimento-pia`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
-
     {
       url: `${baseUrl}/desentupimento-vaso`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
-
     {
       url: `${baseUrl}/desentupimento-esgoto`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
-
     {
       url: `${baseUrl}/desentupimento-ralo`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
-
     {
       url: `${baseUrl}/desentupimento-caixa-gordura`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
-
     {
       url: `${baseUrl}/caca-vazamento`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
-
     {
       url: `${baseUrl}/hidrojateamento`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
-
     {
       url: `${baseUrl}/limpeza-fossa`,
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.9,
     },
-
     {
       url: `${baseUrl}/video-inspecao`,
       lastModified: new Date(),
